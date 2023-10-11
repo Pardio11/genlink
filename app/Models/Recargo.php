fa046f1db33a8292bf11cba190befae3fe221ced
Mauricio Lara <lara.contrerasmauricio@gmail.com>
2023-10-10 22:56:53 -0600

    t descueto descuento recargo

--- app/Models/Recargo.php
+++ app/Models/Recargo.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Recargo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['monto', 'descripcion'];
+
+    public function Recargo(): HasOne
+    {
+        return $this->hasOne(Recargo::class);
+    }
 }
--- app/Models/TipoDescuento.php
+++ app/Models/TipoDescuento.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class TipoDescuento extends Model
 {
-    use HasFactory;
+    protected $fillable = ['nombre', 'monto'];
+ 
+    public function TipoDescuento(): HasOne
+    {
+        return $this->hasOne(TipoDescuento::class);
+    }
 }
