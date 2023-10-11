f90b37cfad1a52f91d36ff3d6cd0cfd7d5a0495f
Pardio11 <l20121022@morelia.tecnm.mx>
2023-10-10 21:25:28 -0600

    Migrate parte 1

---
+++ database/migrations/2023_10_11_032210_create_t_descuentos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_descuentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->decimal('monto', 10, 2); // Usamos el tipo decimal para el monto con 2 decimales
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_descuentos');
+    }
+};
