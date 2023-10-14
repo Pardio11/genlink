0b13cef4ba6775bfd95e539812b70de8ac4411c0
Jesús Vazquez <jesusruso1234@gmail.com>
2023-10-14 17:40:54 -0600

    Seader roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -13,9 +13,25 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
+
         $role1 = Role::create(['name'=>'Admin']);
         $role2 = Role::create(['name'=>'Cliente']);
         $role3 = Role::create(['name'=>'Cobrador']);
+
+        Permission::create(['name'=>'mis-pagos'])->assignRole($role2);
+        Permission::create(['name'=>'preguntas-frecuentes'])->assignRole($role2);
+        Permission::create(['name'=>'reportes'])->assignRole($role2);
+
+        Permission::create(['name'=>'clientes'])->assignRole($role1);
+        Permission::create(['name'=>'estado-cuenta'])->assignRole($role1);
+        Permission::create(['name'=>'pagos-atrasados'])->assignRole($role1);
+        Permission::create(['name'=>'cancelados'])->assignRole($role1);
+        Permission::create(['name'=>'pendientes'])->assignRole($role1);
+        Permission::create(['name'=>'reportes-admin'])->assignRole($role1);
+        
+        Permission::create(['name'=>'realizar-pago'])->assignRole($role3);
+        Permission::create(['name'=>'busqueda-cliente'])->assignRole($role3);
+
 
         
     }
