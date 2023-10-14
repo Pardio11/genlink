81695ec45dc87093db0bd0cd4ce3e0460c2a951e
Pardio11 <l20121022@morelia.tecnm.mx>
2023-10-14 17:29:23 -0600

    Cliente link

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name', 'email', 'password','image_data'
+        'name', 'email', 'password','image_data','cliente_id'
     ];
 
     /**
