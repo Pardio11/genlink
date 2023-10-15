c348205bb6bdf423721a82962fe0268c86b113cb
Mauricio Lara <lara.contrerasmauricio@gmail.com>
2023-10-15 14:30:57 -0600

    profile español y letras

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -1,27 +1,30 @@
 <x-form-section submit="updatePassword">
     <x-slot name="title">
-        {{ __('Update Password') }}
+        {{ __('Actualiza tu contraseña') }}
     </x-slot>
 
     <x-slot name="description">
-        {{ __('Ensure your account is using a long, random password to stay secure.') }}
+        {{ __('Asegurate de usar una contraseña segura para mantener protegida tu cuenta.') }}
     </x-slot>
 
     <x-slot name="form">
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="current_password" value="{{ __('Current Password') }}" />
+            <label for="current_password" class="text-black">{{ __('Contraseña actual') }}</label>
+            {{-- <x-label for="current_password" value="{{ __('Current Password') }}" /> --}}
             <x-input id="current_password" type="password" class="mt-1 block w-full" wire:model="state.current_password" autocomplete="current-password" />
             <x-input-error for="current_password" class="mt-2" />
         </div>
 
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="password" value="{{ __('New Password') }}" />
+            <label for="password" class="text-black">{{ __('Nueva contraseña') }}</label>
+            {{-- <x-label for="password" value="{{ __('New Password') }}" /> --}}
             <x-input id="password" type="password" class="mt-1 block w-full" wire:model="state.password" autocomplete="new-password" />
             <x-input-error for="password" class="mt-2" />
         </div>
 
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+            <label for="password_confirmation" class="text-black">{{ __('Confirmar contraseña') }}</label>
+            {{-- <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" /> --}}
             <x-input id="password_confirmation" type="password" class="mt-1 block w-full" wire:model="state.password_confirmation" autocomplete="new-password" />
             <x-input-error for="password_confirmation" class="mt-2" />
         </div>
@@ -29,11 +32,11 @@
 
     <x-slot name="actions">
         <x-action-message class="mr-3" on="saved">
-            {{ __('Saved.') }}
+            {{ __('Guardado.') }}
         </x-action-message>
 
         <x-button>
-            {{ __('Save') }}
+            {{ __('Guardar') }}
         </x-button>
     </x-slot>
 </x-form-section>
