82b076a1c9bb60e257ced5fa593763d391db6127
Mauricio Lara <lara.contrerasmauricio@gmail.com>
2023-10-14 19:36:40 -0600

    roles y permisos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
     @can('clientes')
         
-    
     @if (Auth::user()->cliente->instalacion == null)
         <div class="contrata ">
             <div class="bg-[#f3f3f3] ml-4 mt-9 w-[20vw] h-[70vh] flex flex-col justify-start items-center"
