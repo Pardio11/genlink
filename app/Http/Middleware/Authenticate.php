fc51525b6a4feee87254316d415afda4559a4035
Jesús Vazquez <jesusruso1234@gmail.com>
2023-10-14 23:47:27 -0600

    a

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -13,7 +13,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        dd(Auth::user());
         return $request->expectsJson() ? null : route('login');
     }
 }
