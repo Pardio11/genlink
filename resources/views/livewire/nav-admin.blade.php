976ca17bb3d7e041694a4db101ca6d46301a7aea
Jesús Vazquez <jesusruso1234@gmail.com>
2023-10-15 00:09:30 -0600

    links nav Admin

--- resources/views/livewire/nav-admin.blade.php
+++ resources/views/livewire/nav-admin.blade.php
@@ -3,24 +3,27 @@
     
 <div class="bg-gray-800 h-screen w-64 text-white sticky top-0 z-30">
     <div class="p-4">
+      
+      
+        <!-- Crear Cuenta -->
+        <a href="{{ route('clientes') }}" :active="request()->routeIs('clientes')" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
+            <span class="text-xl">
+                <i class="fas fa-user-plus"></i>
+            </span>
+            Clientes
+        </a>
+
         <!-- Nuevo -->
-        <a href="#" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
+        <a href="{{ route('agregar-cliente') }}" :active="request()->routeIs('agregar-cliente')" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
             <span class="text-xl">
                 <i class="fas fa-plus-circle"></i>
             </span>
             Nuevo
         </a>
 
-        <!-- Crear Cuenta -->
-        <a href="#" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
-            <span class="text-xl">
-                <i class="fas fa-user-plus"></i>
-            </span>
-            Crear Cuenta
-        </a>
-
+        
         <!-- Reportes -->
-        <a href="#" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
+        <a href="{{ route('reportes-admin') }}" :active="request()->routeIs('reportes-admin')" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
             <span class="text-xl">
                 <i class="fas fa-line-chart"></i>
             </span>
@@ -28,7 +31,7 @@
         </a>
 
         <!-- Pendientes -->
-        <a href="#" class="flex items-center gap-4 py-5 border-b border-gray-700  ">
+        <a href="{{ route('pendientes') }}" :active="request()->routeIs('pendientes')" class="flex items-center gap-4 py-5 border-b border-gray-700  ">
             <span class="text-xl">
                 <i class="fas fa-clock"></i>
             </span>
@@ -36,7 +39,7 @@
         </a>
 
         <!-- Pagos Atrasados -->
-        <a href="#" class="flex items-center gap-4 py-5 border-b  border-gray-700 ">
+        <a href="{{ route('pagos-atrasados') }}" :active="request()->routeIs('pagos-atrasados')"class="flex items-center gap-4 py-5 border-b  border-gray-700 ">
             <span class="text-xl">
                 <i class="fas fa-exclamation-circle"></i>
             </span>
@@ -44,7 +47,7 @@
         </a>
 
         <!-- Servicios Cortados -->
-        <a href="#" class="flex items-center gap-4 py-4 border-b border-gray-700 ">
+        <a href="{{ route('cortados') }}" :active="request()->routeIs('cortados')" class="flex items-center gap-4 py-4 border-b border-gray-700 ">
             <span class="text-xl">
                 <i class="fas fa-cut"></i>
             </span>
@@ -52,7 +55,7 @@
         </a>
 
         <!-- Estado de Cuenta -->
-        <a href="#" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
+        <a href="{{ route('estado-cuenta') }}" :active="request()->routeIs('estado-cuenta')" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
             <span class="text-xl">
                 <i class="fas fa-file-invoice-dollar"></i>
             </span>
@@ -60,7 +63,7 @@
         </a>
 
         <!-- Servicio Cancelado -->
-        <a href="#" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
+        <a href="{{ route('cancelados') }}" :active="request()->routeIs('cancelados')" class="flex items-center gap-4 py-5 border-b border-gray-700 ">
             <span class="text-xl">
                 <i class="fas fa-ban"></i>
             </span>
