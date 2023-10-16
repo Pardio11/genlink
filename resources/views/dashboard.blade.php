c40a06b793fe98bb73bf35806ec45d0887998b6a
Mauricio Lara <lara.contrerasmauricio@gmail.com>
2023-10-15 18:26:21 -0600

    promo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,5 +5,13 @@
         @livewire('HomeStatus')
         @livewire('HomePublicidad')
 
+        
+
     </div>
+    {{-- <div class="bg-black">
+
+        <button class="bg-white hover:bg-blue-700 text-blue-500 font-bold py-2 px-4 rounded-full mb-5 ml-5 mt-5">
+            Contrata ahora
+        </button>
+    </div> --}}
 </x-app-layout>
