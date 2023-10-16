0804f0301a47acf5c8725f2385019e19c70c2c49
Pardio11 <l20121022@morelia.tecnm.mx>
2023-10-16 01:04:30 -0600

    delete cascade

--- database/migrations/2023_10_11_040825_create_clientes_table.php
+++ database/migrations/2023_10_11_040825_create_clientes_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
             $table->string('n_id')->unique();
             $table->string('direccion');
             $table->string('telefono');
-            $table->foreignId('instalacion_id')->nullable()->constrained('instalacions');
-            $table->foreignId('contrato_id')->nullable()->constrained('contratos');
-            $table->foreignId('antena_id')->nullable()->constrained('antenas');
-            $table->foreignId('router_id')->nullable()->constrained('routers');
-            $table->foreignId('zona_id')->nullable()->constrained('zonas');
+            $table->foreignId('instalacion_id')->nullable()->constrained('instalacions')->cascadeOnDelete();
+            $table->foreignId('contrato_id')->nullable()->constrained('contratos')->cascadeOnDelete();
+            $table->foreignId('antena_id')->nullable()->constrained('antenas')->cascadeOnDelete();
+            $table->foreignId('router_id')->nullable()->constrained('routers')->cascadeOnDelete();
+            $table->foreignId('zona_id')->nullable()->constrained('zonas')->cascadeOnDelete();
             $table->timestamps();
         });
     }
