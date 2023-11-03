6cc32cf6ee7bbe5af348008d683bb604a9cdbdcf
Pardio11 <l20121022@morelia.tecnm.mx>
2023-11-03 13:32:27 -0600

    Adelantar meses

--- app/Livewire/BusquedaCliente.php
+++ app/Livewire/BusquedaCliente.php
@@ -2,15 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Caja;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Carbon\Carbon;
 
 #[Layout('layouts.app')]
 
 class BusquedaCliente extends Component
 {
     //public $numeroCliente;
+    public $caja;
     public $dineroCaja = 0;
     public $comision = 0;
     public function calcularCaja()
@@ -27,12 +30,25 @@ class BusquedaCliente extends Component
             ->whereYear('fecha', $anoActual)
             ->whereMonth('fecha', $mesActual)
             ->first();
-        if ($caja != null)
+        $this->caja = $caja;
+        if ($caja != null){
             if ($caja->pagos() != null) {
                 $pagos = $caja->pagos()->with('tipoServicio')->get(); // Obtener todos los pagos con sus tipos de servicio
                 $this->dineroCaja  = $pagos->pluck('tipoServicio.costo')->sum();
                 $this->comision = ($pagos->count() * 5);
             }
+        }
+        else{
+            Caja::create([
+                'fecha' => Carbon::now()->toDateString(), // Esto pondrá la fecha actual en formato 'YYYY-MM-DD'
+                'cobrador_id' => Auth::user()->cobrador->id
+            ]);
+            $caja = Auth::user()->cobrador->cajas()
+            ->whereYear('fecha', $anoActual)
+            ->whereMonth('fecha', $mesActual)
+            ->first();
+        $this->caja = $caja;
+        }
     }
     public function render()
     {
--- resources/views/livewire/realizar-pago.blade.php
+++ resources/views/livewire/realizar-pago.blade.php
@@ -40,7 +40,7 @@
                     <td class="px-6 py-4">1</td>
                     <td class="px-6 py-4">{{ $pago->tipoServicio->nombre }}</td>
                     <td class="px-6 py-4">$@if($pago->recargo)
-                        {{ $pago->recargo->monto }}
+                        {{ $pago->recargo->monto }} @else 0
                     @endif</td>
                     <td class="px-6 py-4">$@if($pago->recargo)
                         {{ $pago->recargo->monto + $pago->tipoServicio->costo}} 
@@ -50,6 +50,7 @@
                     <td class="px-6 py-4">
                         <form action="{{ route('reportarPagos') }}" method="POST">
                             @csrf
+                            <input type="hidden" name="caja_id" id="caja_id" value="{{ $caja_id }}">
                             <input type="hidden" name="pagoId" id="pagoId" value="{{ $pago->id }}">
                             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
                             <svg class="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
@@ -74,16 +75,23 @@
             <option value="{{ $i }}">{{ $i }}</option>
         @endfor
     </select> --}}
-    <div class="container flex row-auto justify-end">
-
-        <button type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
-            <svg class="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 18 21">
-                <path
-                    d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
-            </svg>
-            Pagar
-        </button>
+    <div >
+        <form action="{{ route('adelantarPagos') }}" method="POST" class="container flex flex-row justify-center gap-4">
+            @csrf
+            <input type="hidden" name="caja_id" id="caja_id" value="{{ $caja_id }}">
+            <input type="hidden" name="numeroCliente" id="numeroCliente" value="{{ $cliente->n_id  }}">
+            <div class="flex items-center w-fit">
+                <p class="text-white mr-2">Meses a Adelantar:</p>
+                <input type="number" name="adelanto" id="adelanto" min="1" max="24" step="1" value="1" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg class="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 0 18 21">
+                    <path
+                        d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
+                </svg>
+                Pagar
+            </button>
+        </form>
     </div>
 </div>
