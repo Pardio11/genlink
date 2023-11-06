94b39d53e36e3ecd70815513ac568210fb3b6921
Mauricio Lara <lara.contrerasmauricio@gmail.com>
2023-11-05 23:09:51 -0600

    editar usuarios

--- app/Livewire/Clientes.php
+++ app/Livewire/Clientes.php
@@ -1,29 +1,40 @@
 <?php
 
 namespace App\Livewire;
-
+use App\Models\User;
 use App\Models\Cliente;
+use App\Models\Zona;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 #[Layout('layouts.app')]
 
 class Clientes extends Component
 {
+    
     public $editingCliente;
+    public $idC;
+    
     public $isEditing = false;
-
-  
+    
+    public $clienteId;
+    public $userName;
+    public $userMail;
+    public $userTel;
+    public $userDirec;
+    
 
     public function render()
     {
         $clientes = Cliente::all();
+        $zonas= Zona::all();
 
-        return view('livewire.clientes', ['clientes' => $clientes]);
+        return view('livewire.clientes', ['clientes' => $clientes,'zonas'=>$zonas]);
     }
 
     public function openEditModal($clienteId)
     {
         $this->editingCliente = Cliente::find($clienteId);
+        $this->idC=$this->editingCliente->id;
         $this->isEditing = true;
     }
     
@@ -42,21 +53,28 @@ class Clientes extends Component
             session()->flash('success', 'Registros de pagos y cliente eliminados exitosamente');
         }
     }
+
+
     
-    
-    public function updateCliente()
-{
-    $this->editingCliente->save();
+    public function updateCliente(){
+        $clienteIdd=$this->idC;
+        $cliente=Cliente::find($clienteIdd);
+        $user=User::where('cliente_id', $clienteIdd)->first();
+        $user->name=$this->userName;
+        $user->email=$this->userMail;
+        $cliente->telefono=$this->userTel;
+        $cliente->direccion=$this->userDirec;
+        $cliente->save();
+        $user->save();
+        
     $this->isEditing = false;
     session()->flash('success', 'Cliente actualizado exitosamente');
+
 }
 
 public function closeEditModal()
 {
     $this->isEditing = false;
 }
-
-
-    
 
 }
--- resources/views/livewire/clientes.blade.php
+++ resources/views/livewire/clientes.blade.php
@@ -6,15 +6,28 @@
 
     <div class="w-[79vw] mx-auto p-4 mb-10 h-full">
         <!-- Filtros -->
+        
+<form>   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input name="buscarpor" type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
+        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+    </div>
+</form>
+
         <div class="bg-white p-2 rounded shadow-md mb-4">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
                     <label for="zona" class="block font-semibold">Zona:</label>
-                    <select id="zona" name="zona" class="w-full p-2 border rounded">
-                        <!-- Opciones del select Zona -->
-                        <option value="zona1">Zona 1</option>
-                        <option value="zona2">Zona 2</option>
-                        <option value="zona3">Zona 3</option>
+                    <select wire:model="zona_id" id="modeloAntenaFK" name="zona_id" class="w-1/2 p-2 border rounded block" required>
+                        @foreach($zonas as $zona)
+                            <option value="{{ $zona->id }}">{{ $zona->nombre }}</option>
+                        @endforeach
                     </select>
                 </div>
                 <div>
@@ -36,6 +49,7 @@
                         <th class="px-6 py-3 border-b border-gray-600">Nombre</th>
                         <th class="px-6 py-3 border-b border-gray-600">Correo</th>
                         <th class="px-6 py-3 border-b border-gray-600">Teléfono</th>
+                        <th class="px-6 py-3 border-b border-gray-600">Zona</th>
                         <th class="px-6 py-3 border-b border-gray-600">Dirección</th>
                         <th class="px-6 py-3 border-b border-gray-600">Acciones</th>
                     </tr>
@@ -48,6 +62,7 @@
                         <td class="px-6 py-4">{{$c->user->name}}</td>
                         <td class="px-6 py-4">{{$c->user->email}}</td>
                         <td class="px-6 py-4">{{ $c->telefono }}</td>
+                        <td class="px-6 py-4">{{ $c->zona->nombre }}</td>
                         <td class="px-6 py-4">{{ $c->direccion }}</td>
                         <td class="px-6 py-4">
                             <div class="flex-col ">
@@ -72,31 +87,36 @@
             <!-- Modal Panel -->
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
             <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full sm:p-6">
+                
                 <div>
                     <h2 class="text-lg font-semibold mb-4">Editar Cliente</h2>
-                    <form wire:submit.prevent="updateCliente">
+                    <form wire:submit.prevent="updateCliente">                        
+                        <input wire:model="idC" type="hidden" id="idC" name="idC" >
+                        
+                        
                         <div>
                             <label for="nombre" class="block font-semibold">Nombre:</label>
-                            <input wire:model="clienteSeleccionado.user.name" type="text" id="nombre" name="nombre" class="w-full p-2 border rounded">
+                            <input wire:model="userName" type="text" id="userName" name="userName" class="w-full p-2 border rounded">
                         </div>
                         <div>
                             <label for="correo" class="block font-semibold">Correo:</label>
-                            <input wire:model="editingCliente.user.email" type="text" id="correo" name="correo" class="w-full p-2 border rounded">
+                            <input wire:model="userMail" type="text" id="userMail" name="userMail" class="w-full p-2 border rounded">
                         </div>
                         <div>
                             <label for="telefono" class="block font-semibold">Teléfono:</label>
-                            <input wire:model="editingCliente.telefono" type="text" id="telefono" name="telefono" class="w-full p-2 border rounded">
+                            <input wire:model="userTel" type="text" id="userTel" name="userTel" class="w-full p-2 border rounded">
                         </div>
                         <div>
                             <label for="direccion" class="block font-semibold">Dirección:</label>
-                            <input wire:model="editingCliente.direccion" type="text" id="direccion" name="direccion" class="w-full p-2 border rounded">
+                            <input wire:model="userDirec" type="text" id="userDirec" name="userDirec" class="w-full p-2 border rounded">
                         </div>
                         
                         <div class="mt-4">
                             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold w-20 h-8 rounded">Guardar</button>
-                            <button wire:click="closeEditModal" class="bg-gray-400 hover:bg-gray-600 text-white font-bold w-20 h-8 rounded ml-2">Cancelar</button>
                         </div>
                     </form>
+
+                    <button wire:click="closeEditModal" class="bg-gray-400 hover:bg-gray-600 text-white font-bold w-20 h-8 rounded ml-2">Cancelar</button>
                 </div>
             </div>
         </div>
